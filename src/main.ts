b44722a4b57c9d311342c2aa4bebffaa2d15adb2
Ben Coleman <benc.uk@gmail.com>
2024-03-01 17:10:38 +0000

    🗺️ Layout improvements

--- src/main.ts
+++ src/main.ts
@@ -12,33 +12,39 @@ async function startApp() {
 
   document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <main>
-  <div class="flexrow">
-    <span class="title">Tomb Raider - Level Viewer</span>
-    <select id="levelSelect">
-      <option value="Tomb-Raider-1/01-Caves.PHD">01 Caves</option>
-      <option value="Tomb-Raider-1/02-City-of-Vilcabamba.PHD">02 City of Vilcabamba</option>
-      <option value="Tomb-Raider-1/03-The-Lost-Valley.PHD">03 The Lost Valley</option>
-      <option value="Tomb-Raider-1/04-Tomb-of-Qualopec.PHD">04 Tomb of Qualopec</option>
-      <option value="Tomb-Raider-1/05-St-Francis-Folly.PHD">05 St Francis' Folly</option>
-      <option value="Tomb-Raider-1/06-Colosseum.PHD">06 Colosseum</option>
-      <option value="Tomb-Raider-1/07-Palace-Midas.PHD">07 Palace Midas</option>
-      <option value="Tomb-Raider-1/08-Cistern.PHD">08 The Cistern</option>
-      <option value="Tomb-Raider-1/09-Tomb-of-Tihocan.PHD">09 Tomb of Tihocan</option>
-      <option value="Tomb-Raider-1/10-City-of-Khamoon.PHD">10 City of Khamoon</option>
-      <option value="Tomb-Raider-1/11-Obelisk-of-Khamoon.PHD">11 Obelisk of Khamoon</option>
-      <option value="Tomb-Raider-1/12-Sanctuary-of-the-Scion.PHD">12 Sanctuary of the Scion</option>
-      <option value="Tomb-Raider-1/13-Natlas-Mines.PHD">13 Natla's Mines</option>
-      <option value="Tomb-Raider-1/14-Atlantis.PHD">14 Atlantis</option>
-      <option value="Tomb-Raider-1/15-The-Great-Pyramid.PHD">15 The Great Pyramid</option>
-      <option value="Tomb-Raider-1/00-Laras-Home.PHD">00 Laras Home</option>
-    </select>
-  </div>
-
+  <select id="levelSelect">
+    <option value="Tomb-Raider-1/01-Caves.PHD">01 Caves</option>
+    <option value="Tomb-Raider-1/02-City-of-Vilcabamba.PHD">02 City of Vilcabamba</option>
+    <option value="Tomb-Raider-1/03-The-Lost-Valley.PHD">03 The Lost Valley</option>
+    <option value="Tomb-Raider-1/04-Tomb-of-Qualopec.PHD">04 Tomb of Qualopec</option>
+    <option value="Tomb-Raider-1/05-St-Francis-Folly.PHD">05 St Francis' Folly</option>
+    <option value="Tomb-Raider-1/06-Colosseum.PHD">06 Colosseum</option>
+    <option value="Tomb-Raider-1/07-Palace-Midas.PHD">07 Palace Midas</option>
+    <option value="Tomb-Raider-1/08-Cistern.PHD">08 The Cistern</option>
+    <option value="Tomb-Raider-1/09-Tomb-of-Tihocan.PHD">09 Tomb of Tihocan</option>
+    <option value="Tomb-Raider-1/10-City-of-Khamoon.PHD">10 City of Khamoon</option>
+    <option value="Tomb-Raider-1/11-Obelisk-of-Khamoon.PHD">11 Obelisk of Khamoon</option>
+    <option value="Tomb-Raider-1/12-Sanctuary-of-the-Scion.PHD">12 Sanctuary of the Scion</option>
+    <option value="Tomb-Raider-1/13-Natlas-Mines.PHD">13 Natla's Mines</option>
+    <option value="Tomb-Raider-1/14-Atlantis.PHD">14 Atlantis</option>
+    <option value="Tomb-Raider-1/15-The-Great-Pyramid.PHD">15 The Great Pyramid</option>
+    <option value="Tomb-Raider-1/00-Laras-Home.PHD">00 Laras Home</option>
+  </select>
+ 
   <canvas id="canvas" 
    width="${config.width}" 
    height="${config.width * config.aspectRatio}" 
    style="${canvasStyle}">
   </canvas>
+
+  <div id="help">
+    <p>Press H to show or hide this help</p>
+    <p>Use the arrow keys or WASD to move</p>
+    <p>Use the mouse to look around</p>
+    <p>Use 1 & 2 to change the global light angle</p>
+    <p>Use 3 & 4 to change the global light height</p>
+    <p>Use [ & ] keys to move up & down</p>
+  </div>
 </main>`
 
   const ctx = await Context.init()
@@ -70,6 +76,10 @@ async function startApp() {
     if (e.key === '4') {
       globalLightHeight += 0.1
     }
+    if (e.key === 'h') {
+      document.querySelector<HTMLDivElement>('#help')!.style.display =
+        document.querySelector<HTMLDivElement>('#help')!.style.display === 'none' ? 'block' : 'none'
+    }
 
     if (globalLightHeight < 0.1) {
       globalLightHeight = 0.1
@@ -84,6 +94,10 @@ async function startApp() {
 
   // Trigger the level select to load the first level
   document.querySelector('#levelSelect')!.dispatchEvent(new Event('change'))
+
+  setTimeout(() => {
+    document.querySelector<HTMLDivElement>('#help')!.style.display = 'none'
+  }, 3000)
 }
 
 // =============================================================================
