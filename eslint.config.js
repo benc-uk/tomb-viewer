5ea1a582393b82b8953c7fc985002438540778f0
Ben Coleman <benc.uk@gmail.com>
2024-03-08 10:44:15 +0000

    🦄 Some renaming/refactoring

--- eslint.config.js
+++ eslint.config.js
@@ -20,6 +20,7 @@ export default tseslint.config(eslint.configs.recommended, ...tseslint.configs.r
       'error',
       {
         destructuredArrayIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
       },
     ],
   },
